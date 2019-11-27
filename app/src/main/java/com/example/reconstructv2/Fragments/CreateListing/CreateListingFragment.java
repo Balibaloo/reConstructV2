59c0f1b6c189caa4416f5a05fdc7b170924e259d
Roman <romankubiv101@gmail.com>
2019-11-27 20:43:45 +0000

    implemented all api methods

---
+++ app/src/main/java/com/example/reconstructv2/Fragments/CreateListing/CreateListingFragment.java
@@ -0,0 +1,56 @@
+package com.example.reconstructv2.Fragments.CreateListing;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.reconstructv2.R;
+
+
+public class CreateListingFragment extends Fragment {
+
+    private CreateListingFragment.OnFragmentInteractionListener mListener;
+    private CreateListingViewModel viewModel;
+
+    public CreateListingFragment() {}
+
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_create_listing,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof CreateListingFragment.OnFragmentInteractionListener) {
+            mListener = (CreateListingFragment.OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener{
+        void onCreateListingFragmentInteraction(Uri uri);
+    }
+
+
+}
